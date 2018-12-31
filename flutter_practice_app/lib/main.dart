efc6866b3a4816f1bf71828dffec9bbba8f9c306
chanM0 <mikec201722@gmail.com>
2018-12-30 21:04:29 -0800

    Created a statefulWidget

--- flutter_practice_app/lib/main.dart
+++ flutter_practice_app/lib/main.dart
@@ -7,9 +7,20 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  // Strickly typing
-  // Overrides because there is a build method within StatelessWidget
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+/// adding the _ClassName makes the class private essentially
+/// cant import the class to another file
+/// State<MyApp>
+class _MyAppState extends State<MyApp> {
+// Strickly typing
+// Overrides because there is a build method within StatelessWidget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
