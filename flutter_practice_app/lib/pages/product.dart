1684fa70142e85403f78a4727155832f310d9e89
chanM0 <mikec201722@gmail.com>
2019-01-01 19:24:59 -0800

    69. Improving the Look of the Product Page added new UI fixes

--- flutter_practice_app/lib/pages/product.dart
+++ flutter_practice_app/lib/pages/product.dart
@@ -4,17 +4,27 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Product Detail'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Text('Details!'),
-            RaisedButton(
-              child: Text("BACK!"),
-              onPressed: () => Navigator.pop(context),
-            )
-          ],
-        ));
+      appBar: AppBar(
+        title: Text('Product Detail'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('assets/film_cart.jpeg'),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('Details!'),
+          ),
+          Container(
+              padding: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                color: Theme.of(context).accentColor,
+                child: Text("BACK!"),
+                onPressed: () => Navigator.pop(context),
+              ))
+        ],
+      ),
+    );
   }
 }
